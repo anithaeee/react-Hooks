98c9c12719492b74c13c254dbaf44b64d3adef0c
anithaeee <anithaeee224@gmail.com>
2023-12-17 22:29:39 +0800

    dec17

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+  const[count,setcount] = useState(0);
+   useEffect(()=>{
+     console.log("clicked");
+   },[count])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='container'>
+         <h1>{count}</h1>
+         <button onClick={(()=>setcount(count+1))}>Click</button>
+       </div>
     </div>
   );
 }
